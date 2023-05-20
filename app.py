4dbc63256cfea6db13fe3966c2b73dc85bd503f9
Anjali Panchariya <anjalipanchariya11@gmail.com>
2023-05-20 11:29:41 +0530

    app update

--- app.py
+++ app.py
@@ -32,7 +32,7 @@ app.secret_key = os.urandom(24)
 
 app.config['MYSQL_HOST'] = 'localhost'
 app.config['MYSQL_USER'] = 'root'
-app.config['MYSQL_PASSWORD'] = 'Anjali@11'
+app.config['MYSQL_PASSWORD'] = ''
 app.config['MYSQL_DB'] = 'loginreg'
 
 mysql = MySQL(app)
